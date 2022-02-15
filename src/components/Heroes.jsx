868a902ef52342e6114927acb1d30c9d005bc7ff
Pablo Marino <pmarino2013@gmail.com>
2022-02-14 22:51:04 -0300

    Estilos y eventos

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -4,13 +4,34 @@ const Heroes = (props) => {
   const { heroes } = props;
   //   console.log(heroes);
 
+  const handleClick = (id) => {
+    alert(
+      `Tu héroe seleccionado es ${heroes[id].nombre} y pertenece a la editorial ${heroes[id].owner}`
+    );
+  };
+
   return (
     <div>
       <h3>Lista de Héroes</h3>
       <ul>
-        {heroes.map((heroe) => (
-          <li>
-            {heroe.nombre} - {heroe.owner}
+        {heroes.map((heroe, index) => (
+          <li
+            key={index}
+            className="item"
+            onClick={() => {
+              handleClick(index);
+            }}
+          >
+            {heroe.nombre} -
+            <span
+              style={{
+                color: heroe.owner === "DC" ? "blue" : "red",
+                fontFamily: "fantasy",
+                backgroundColor: "gold",
+              }}
+            >
+              {heroe.owner}
+            </span>
           </li>
         ))}
       </ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./css/estilo.css";
+
 import App from "./App";
 
 // const elemento = <h1>Hola React!!</h1>;
